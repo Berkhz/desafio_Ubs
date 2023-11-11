3dd8d2cc83ea3b18b960b7bb1f3917f8fe81021a
Kauan Bertalha <kauan.hb2004@gmail.com>
2023-11-11 18:58:12 -0300

    Adicionando propriedades web

---
+++ projeto/src/main/java/com/ubsdigital/source/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package com.ubsdigital.source.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    public IndexController() {
+
+    }
+
+    public void Home() {
+
+    }
+}
---
+++ projeto/src/main/java/com/ubsdigital/source/controllers/LoginController.java
@@ -0,0 +1,13 @@
+package com.ubsdigital.source.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
