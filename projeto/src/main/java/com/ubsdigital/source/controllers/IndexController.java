0b5d95f633fb6822f209fa5caca1b1300a1ad457
Kauan Bertalha <kauan.hb2004@gmail.com>
2023-11-11 19:31:16 -0300

    Configurações web

--- projeto/src/main/java/com/ubsdigital/source/controllers/IndexController.java
+++ projeto/src/main/java/com/ubsdigital/source/controllers/IndexController.java
@@ -6,11 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-    public IndexController() {
-
-    }
-
-    public void Home() {
-
+    @GetMapping("/home")
+    public String home() {
+        return "index/home";
     }
 }
